a3a63b4732247d5f06453814697b8d57d3e16e4a
Eliya kaheni <89583292+EliyaKaheni@users.noreply.github.com>
2024-12-18 12:59:13 +0330

    DataBaseManager created

--- AITown-Back/Back.py
+++ AITown-Back/Back.py
@@ -1,4 +1,7 @@
-from GPT import prompt
+from mysql.connector import Error
+from typing import Optional, Dict
+from GPT import pro
+import mysql.connector
 
 class UserManagement:
     def signup(username:str, password:str) -> bool:
@@ -47,14 +50,99 @@ class CourtRoom:
         pass
 
 class DataBaseManager:
-    def login(username:str, password:str) -> bool:
-        pass
+    def __init__(self):
+        self.host = 'localhost'
+        self.user = 'root'
+        self.password = 'Ekaheni82'
+        self.database = 'aiTown'
+        self.connection = None
 
-    def store_court_case(court_case:CourtRoom) -> bool:
-        pass
+    def connect(self):
+        try:
+            self.connection = mysql.connector.connect(
+                host = self.host,
+                user = self.user,
+                password = self.password,
+                database = self.database
+            )
+        
+        except Error as e:
+            print(f'Error occured in connecting to database: {e}')
 
-    def store_user(username:str, password:str) -> bool:
-        pass
 
-    def get_recent_cases(username:str) -> dict:
-        pass
+    def close_connection(self):
+        if self.connection:
+            self.connection.close()
+
+
+    def login(self, username:str, password:str) -> bool:
+        query = 'SELECT * FROM users WHERE username = %s AND password = %s'
+        try:
+            self.connect()
+            cursor = self.connection.cursor(dictionary=True)
+            cursor.execute(query, (username, password))
+            user = cursor.fetchone()
+            return user is not None
+        
+        except Error as e:
+            print(f'Error occured during login: {e}')
+            return False
+        
+        finally:
+            self.close_connection()
+
+
+    def store_court_case(self, court_case:CourtRoom) -> bool:
+        query = '''INSERT INTO courtCaseRecord (sername, conversations)
+        VALUES (%s, %s)'''
+        try:
+            self.connect()
+            cursor = self.connection.cursor()
+            cursor.execute(query, court_case.username, court_case.case_history)
+            self.connection.commit()
+            return True
+        
+        except Error as e:
+            print(f'Error occured during storing court case: {e}')
+            return False
+        
+        finally:
+            self.close_connection()
+
+
+    def store_user(self, username:str, password:str) -> bool:
+        query = 'INSERT INTO users (username, password) VALUES (%s, %s)'
+        try:
+            self.connect()
+            cursor = self.connection.cursor()
+            cursor.execute(query, (username, password))
+            self.connection.commit()
+            return True
+        
+        except Error as e:
+            print(f'Error occured during storing court case: {e}')
+            return False
+        
+        finally:
+            self.close_connection()
+
+
+    def get_recent_cases(self, username: str) -> Optional[Dict]:
+        query = """SELECT * FROM courtCaseRecord 
+        WHERE username = %s 
+        ORDER BY created_at DESC
+        LIMIT 10
+        """
+        try:
+            self.connect()
+            cursor = self.connection.cursor(dictionary=True)
+            cursor.execute(query, (username,))
+            cases = cursor.fetchall()
+            return cases if cases else {}
+        
+        except Error as e:
+            print(f"Error retrieving recent cases: {e}")
+            return {}
+        
+        finally:
+            self.close_connection()
